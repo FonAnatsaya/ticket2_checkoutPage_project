fc60838f5c007a5351dc50df78b030312153e116
fon <anatsaya.lee@gmail.com>
2024-05-02 02:17:15 +0700

    Finish Tickets component layout

--- src/components/Tickets.tsx
+++ src/components/Tickets.tsx
@@ -1,5 +1,26 @@
+import "./Tickets.css";
 const Tickets = () => {
-  return <div>TICKETS</div>;
+  return (
+    <div className="Tickets">
+      <div className="Tickets__TopLabel">Tickets</div>
+      <div className="Tickets__Item1">
+        <div className="Tickets__Item1__LeftDetail">
+          <div className="Tickets__Item1__LeftDetail__IMG">IMG</div>
+          <div>
+            <h2>Siam Amazing Park</h2>
+            <p>Siam Amazing Park Everyday is Amazing</p>
+            <p>สยามอะเมซิ่งเป็นอาณาจักรแห่งความสุขสนุกไม่รู้ลืม...</p>
+          </div>
+        </div>
+        <div className="Tickets__Item1__RightDetail">
+          <h2>1,000 THB</h2>
+          <div className="Tickets__Item1__RightDetail__Button">
+            <button>Add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Tickets;
