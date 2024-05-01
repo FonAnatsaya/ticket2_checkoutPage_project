164b73915cd000ce105e1a1bd41fe2a5313c9768
fon <anatsaya.lee@gmail.com>
2024-05-02 00:09:56 +0700

    Finish Header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
 import "./Header.css";
 
 const Header = () => {
-  return <div className="Header">Header</div>;
+  return (
+    <div className="Header">
+      <div className="Header__Logo">
+        <div>LOGO</div>
+      </div>
+      <h2 className="Header__Name">Ticket2Attraction</h2>
+    </div>
+  );
 };
 
 export default Header;
