2c5fcf6bfbd53241cb2721fe073a24220d0f2de2
fon <anatsaya.lee@gmail.com>
2024-05-07 00:14:12 +0700

    Create getDiscount service

--- src/service/serviceTicket.ts
+++ src/service/serviceTicket.ts
@@ -10,3 +10,12 @@ export const getTickets = ()=> {
         console.error("Error: ", error);
       });
 }
+
+export const getDiscount = (discountCode: string, totalPrice:number)=>{
+  const discountData = {discountCode, totalPrice};
+  return axios.post("http://localhost:8000/discount", discountData).then((response)=>{
+    return response.data.discount;
+  }).catch((error)=>{
+    console.error("Error: ", error);
+  })
+}
